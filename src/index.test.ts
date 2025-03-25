e2e64e892122b3377949d397ccc08634a30dc953
Prawee Wongsa <prawee@hotmail.com>
2025-03-25 12:43:24 +0700

    test add func

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,10 @@
 import { expect, test } from "vitest";
-import { hello } from "./index";
+import { hello, add } from "./index";
 
 test("hello function", () => {   
     expect(hello("Pod")).toBe("hello Pod");
+});
+
+test("test add func with 1+2 = 3", () => {
+    expect(add(1, 2)).toBe(3);
 });
